444255381e1c9fd53abf61f005ec4e06223ac58c
Your Name <you@example.com>
2022-07-22 11:29:19 +0530

    CountDown using JS

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date("July 22 , 2023 00:00:00").getTime();
+    const countDate = new Date("July 22 , 2025 00:00:00").getTime();
     const now = new Date().getTime();
     const gap = countDate-now;
     console.log(gap);
